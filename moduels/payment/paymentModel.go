3a34d5278c4a4914fa9dd460603fa86dd735fc7f
Noei <35981528+hifat@users.noreply.github.com>
2024-02-18 16:22:11 +0700

    feat: add domain (#4)

--- moduels/payment/paymentModel.go
+++ moduels/payment/paymentModel.go
@@ -1,1 +1,12 @@
 package payment
+
+type (
+	ItemServiceReqDatum struct {
+		ItemId string  `json:"item_id" validate:"required,max=64"`
+		Price  float64 `json:"price"`
+	}
+
+	ItemServiceReq struct {
+		Items []*ItemServiceReqDatum `json:"items" validate:"required"`
+	}
+)
