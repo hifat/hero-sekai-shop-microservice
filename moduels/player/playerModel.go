c8208b58514a2df1422137d3395b1d321d2e4eae
Noei <35981528+hifat@users.noreply.github.com>
2024-02-18 15:12:09 +0700

    feat: add model, auth model (#3)
    * feat: add model
    
    * feat: add auth domain

--- moduels/player/playerModel.go
+++ moduels/player/playerModel.go
@@ -1,1 +1,13 @@
 package player
+
+import "time"
+
+type (
+	PlayerProfile struct {
+		Id        string    `json:"id"`
+		Email     string    `json:"email"`
+		Username  string    `json:"username"`
+		CreatedAt time.Time `json:"created_at"`
+		UpdatedAt time.Time `json:"updated_at"`
+	}
+)
