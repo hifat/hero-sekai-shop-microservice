e1107366799f3aa91e1f66e688e5cf5e127511a8
Noei <35981528+hifat@users.noreply.github.com>
2024-02-20 01:32:47 +0700

    feat: add grpc pkg (#6)

---
+++ pkg/grpccon/grpccon.go
@@ -0,0 +1,74 @@
+package grpccon
+
+import (
+	"net"
+
+	"gitnub.com/hifat/hero-sekai-shop-microservice/config"
+	"gitnub.com/hifat/hero-sekai-shop-microservice/moduels/auth/authProto"
+	"gitnub.com/hifat/hero-sekai-shop-microservice/moduels/inventory/inventoryProto"
+	"gitnub.com/hifat/hero-sekai-shop-microservice/moduels/item/itemProto"
+	"gitnub.com/hifat/hero-sekai-shop-microservice/moduels/player/playerProto"
+	"gitnub.com/hifat/hero-sekai-shop-microservice/pkg/logger"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+type (
+	GrpcClientFactoryHandler interface {
+		Auth() authProto.AuthGrpcServiceClient
+		Player() playerProto.PlayerGrpcServiceClient
+		Item() itemProto.ItemGrpcServiceClient
+		Inventory() inventoryProto.InventoryGrpcServiceClient
+	}
+
+	grpcClientFactory struct {
+		client *grpc.ClientConn
+	}
+
+	grpcAuth struct{}
+)
+
+func (g *grpcClientFactory) Auth() authProto.AuthGrpcServiceClient {
+	return authProto.NewAuthGrpcServiceClient(g.client)
+}
+
+func (g *grpcClientFactory) Player() playerProto.PlayerGrpcServiceClient {
+	return playerProto.NewPlayerGrpcServiceClient(g.client)
+}
+
+func (g *grpcClientFactory) Item() itemProto.ItemGrpcServiceClient {
+	return itemProto.NewItemGrpcServiceClient(g.client)
+}
+
+func (g *grpcClientFactory) Inventory() inventoryProto.InventoryGrpcServiceClient {
+	return inventoryProto.NewInventoryGrpcServiceClient(g.client)
+}
+
+func NewGrpcClient(host string) (GrpcClientFactoryHandler, error) {
+	opts := make([]grpc.DialOption, 0)
+
+	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	clientConn, err := grpc.Dial(host, opts...)
+	if err != nil {
+		logger.Error(err.Error())
+		return nil, err
+	}
+
+	return &grpcClientFactory{
+		client: clientConn,
+	}, nil
+}
+
+func NewGrpcServer(cfg *config.Jwt, host string) (*grpc.Server, net.Listener) {
+	opts := make([]grpc.ServerOption, 0)
+
+	grpcServer := grpc.NewServer(opts...)
+
+	lis, err := net.Listen("tpc", host)
+	if err != nil {
+		logger.Fatal(err.Error())
+	}
+
+	return grpcServer, lis
+}
